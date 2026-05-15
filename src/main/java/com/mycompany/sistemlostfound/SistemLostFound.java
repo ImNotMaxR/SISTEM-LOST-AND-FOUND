dc98c000c286ff33995decf4b5ce10ec8e922057
MaxR <MaxR@DESKTOP-A9AK3I4>
2026-05-15 11:49:26 +0700

    update v1

--- src/main/java/com/mycompany/sistemlostfound/SistemLostFound.java
+++ src/main/java/com/mycompany/sistemlostfound/SistemLostFound.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sistemlostfound;
 
-/**
- *
- * @author MaxR
- */
-public class SistemLostFound {
 
+public class SistemLostFound{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Dunia!");
     }
 }
