032c08f86ed1d9c9aa5510287027f0c8f2bb6dde
FadilFauzi-SE <riksanriyadif@gmail.com>
2026-05-31 18:09:30 +0700

    Update Category.java

--- src/main/java/com/model/Category.java
+++ src/main/java/com/model/Category.java
@@ -9,5 +9,31 @@ package com.model;
  * @author MaxR
  */
 public class Category {
-    
+    private String userId;
+    private String name;
+    private boolean requestVerification;
+
+    public Category(String userId, String name, boolean requestVerification) {
+        this.userId = userId;
+        this.name = name;
+        this.requestVerification = requestVerification;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVerificationRequired() {
+        return requestVerification;
+    }
+
+    @Override
+    public String toString() {
+        return "Category: " + name +
+               " | Verification: " + requestVerification;
+    }
 }
