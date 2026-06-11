8efc559737af20fa82a3f0137b7eb6d815e94042
MaxR <MaxR@DESKTOP-A9AK3I4>
2026-06-12 00:33:20 +0700

    konekin database

--- src/main/java/com/mycompany/sistemlostfound/SistemLostFound.java
+++ src/main/java/com/mycompany/sistemlostfound/SistemLostFound.java
@@ -1,9 +1,14 @@
 package com.mycompany.sistemlostfound;
 
 import com.database.DBConnection;
+import java.sql.*;
 
 public class SistemLostFound {
     public static void main(String[] args) {
-        DBConnection.getInstance();
+        DBConnection db = DBConnection.getInstance();
+        Connection conn = db.getConnection();
+        if (conn != null) {
+            System.out.println("Koneksi sukses!");
+        }
     }
 }
